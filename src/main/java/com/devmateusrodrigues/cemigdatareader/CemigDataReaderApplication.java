0ae279e3d789d49ae3a3a051db06f55e13a7a0c8
Mateus Santos <mhsrtindora@gmail.com>
2022-01-10 19:14:15 -0300

    cemig-data-reader first commit

---
+++ src/main/java/com/devmateusrodrigues/cemigdatareader/CemigDataReaderApplication.java
@@ -0,0 +1,13 @@
+package com.devmateusrodrigues.cemigdatareader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemigDataReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemigDataReaderApplication.class, args);
+	}
+
+}
